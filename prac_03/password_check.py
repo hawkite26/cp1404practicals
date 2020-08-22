fbc6b80dedd5409cc96f81f4172406c6e2f5781d
Kye Bryce <kye-bryce@hotmail.com>
2020-08-22 20:41:16 +1000

    Refactor password checker program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -3,11 +3,20 @@
 
 def main():
     minimum_length = 7
+    password = get_password(minimum_length)
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("Your password is ", len(password) * "*", " long.", sep="")
+
+
+def get_password(minimum_length):
     password = input("Please enter a username with at least {} characters: ".format(minimum_length))
     while len(password) < minimum_length:
         print("Invalid length.")
         password = input("Please enter a username with at least {} characters: ".format(minimum_length))
-    print("Your password is ", len(password) * "*", " long.", sep="")
+    return password
 
 
 main()
