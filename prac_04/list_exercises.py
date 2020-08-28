d52e4e2041f31b06ad64bb496026010a40fab285
Kye Bryce <kye-bryce@hotmail.com>
2020-08-28 17:16:00 +1000

    Add username verification check

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,8 +3,12 @@ CP1404/CP5632 Practical
 Numbers list program
 """
 
+USERNAMES = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
 
 def main():
+    check_valid_username(USERNAMES)
     numbers = []
     for i in range(5):
         number = check_valid_number()
@@ -14,6 +18,17 @@ def main():
     print_number_info("smallest", min(numbers))
     print_number_info("largest", max(numbers))
     print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+
+
+def check_valid_username(usernames):
+    valid_username = False
+    while not valid_username:
+        username = input("Please enter username: ")
+        if username in usernames:
+            print("Access Granted")
+            valid_username = True
+        else:
+            print("Access Denied")
 
 
 def check_valid_number():
