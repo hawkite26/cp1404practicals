941dfae23fc8b4937d7ae1bb11c6f1354c3b4011
Kye Bryce <kye-bryce@hotmail.com>
2020-08-28 16:44:32 +1000

    Refactor valid number check into function

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -7,15 +7,20 @@ Numbers list program
 def main():
     numbers = []
     for i in range(5):
-        valid_entry = False
-        while not valid_entry:
-            try:
-                number = int(input("Please enter a number: "))
-                numbers.append(number)
-                valid_entry = True
-            except ValueError:
-                print("That is an invalid entry.")
-    return numbers
+        number = check_valid_number()
+        numbers.append(number)
+    print(numbers)
+
+
+def check_valid_number():
+    valid_entry = False
+    while not valid_entry:
+        try:
+            number = int(input("Please enter a number: "))
+            valid_entry = True
+        except ValueError:
+            print("That is an invalid entry.")
+    return number
 
 
 main()
