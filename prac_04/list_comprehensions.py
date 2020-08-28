26c988c321bc9f74d53afbec143e8f59138e460d
Kye Bryce <kye-bryce@hotmail.com>
2020-08-28 17:25:19 +1000

    Complete full_numbers to numbers almost_numbers list TODO

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -32,9 +32,9 @@ lowercase_full_names = [full_name.lower() for full_name in full_names]
 print(lowercase_full_names)
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
-# TODO: use a list comprehension to create a list of integers
-# from the above list of strings
-# numbers =
+# create a list of integers from the above list of strings
+numbers = [int(number) for number in almost_numbers]
+print(numbers)
 
 # TODO: use a list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
