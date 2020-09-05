e3c4f8e658c09a087c55479acdf8268cb1b55fc1
Kye Bryce <kye-bryce@hotmail.com>
2020-09-05 21:21:21 +1000

    Add user input loop and validity checking with appropriate output for hex_colours.py

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -11,6 +11,12 @@ COLOUR_TO_HEX = {"white": "#ffffff", "black": "#000000", "blue1": "#0000ff", "re
 def main():
     display_valid_colours()
     chosen_colour = input("Please enter a colour name: ").lower()
+    while chosen_colour != "":
+        if chosen_colour in COLOUR_TO_HEX:
+            print("{} is {}". format(chosen_colour, COLOUR_TO_HEX[chosen_colour]))
+        else:
+            print("Invalid colour")
+        chosen_colour = input("Please enter a colour name: ").lower()
 
 
 def display_valid_colours():
