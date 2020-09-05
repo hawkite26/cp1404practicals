3fc8b964915b62768b1fdbf884abc8baed01a14a
Kye Bryce <kye-bryce@hotmail.com>
2020-09-05 22:57:09 +1000

    Add preliminary code to check name and add it to dictionary

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,9 +5,21 @@ Email storing system
 
 
 def main():
+    email_to_name = {}
     user_email = input("Email: ")
-    possible_name = find_possible_name(user_email)
-    print(possible_name)
+    while user_email != "":
+        possible_name = find_possible_name(user_email)
+        user_answer = input("Is your name {}? (Y/n) ".format(possible_name)).lower()
+        while user_answer != 'y' and user_answer != 'n' and user_answer != '':
+            print("That is an invalid answer")
+            user_answer = input("Is your name {}? (Y/n) ".format(possible_name)).lower()
+        if user_answer == '' or user_answer == 'y':
+            email_to_name[user_email] = possible_name
+        elif user_answer == 'n':
+            name = input("Name: ").title()
+            email_to_name[user_email] = name
+        user_email = input("Email: ")
+    print(email_to_name)
 
 
 def find_possible_name(user_email):
@@ -15,7 +27,7 @@ def find_possible_name(user_email):
     email_name = user_email[:stop_index]
     first_last_name = email_name.split('.')
     if len(first_last_name) == 1:
-        possible_name = str(first_last_name[0])
+        possible_name = str(first_last_name[0]).title()
     else:
         possible_name = "{} {}".format(first_last_name[0], first_last_name[-1]).title()
     return possible_name
