f05749e87031effaf4e89dc6cbb9cc33b2ebc310
Kye Bryce <kye-bryce@hotmail.com>
2020-09-05 21:50:21 +1000

    Refactor dictionary creator into separate function and add sorting to print function

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -4,17 +4,27 @@ Word occurrences counter
 """
 
 
+from operator import itemgetter
+FIRST_LETTER = 0
+
+
 def main():
-    word_to_count = {}
-    print("Please enter a sentence or string")
-    chosen_string = input(">>> ")
+    chosen_string = input("Text: ").lower()
     words = chosen_string.split()
+    words.sort()
+    word_to_count = count_words(words)
+    for word in word_to_count:
+        print("{} : {}".format(word, word_to_count[word]))
+
+
+def count_words(words):
+    word_to_count = {}
     for word in words:
         if word in word_to_count:
             word_to_count[word] += 1
         else:
             word_to_count[word] = 1
-    print(word_to_count)
+    return word_to_count
 
 
 main()
