09d4dd55c5397a47f3a27efd9503dbab2d669384
Kye Bryce <kye-bryce@hotmail.com>
2020-09-11 17:30:21 +1000

    Update languages.py to print dynamically typed languages

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -4,4 +4,15 @@ ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-print(ruby)
+dynamic_languages = []
+if ruby.is_dynamic():
+    dynamic_languages.append(ruby.name)
+if python.is_dynamic():
+    dynamic_languages.append(python.name)
+if visual_basic.is_dynamic():
+    dynamic_languages.append(visual_basic.name)
+
+print("The dynamically typed languages are:")
+for language in dynamic_languages:
+    print(language)
+
