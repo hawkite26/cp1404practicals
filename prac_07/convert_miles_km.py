82ff7fbe9f528a7cb93d46e4f3a19ab9c86bc493
Kye Bryce <kye-bryce@hotmail.com>
2020-09-27 20:58:12 +1000

    Change convert_miles_km.py and .kv to be more concise and attempt to change input_value

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -13,16 +13,16 @@ class MilesKilometreConverterApp(App):
         return self.root
 
     def handle_calculate(self, value):
+        print('convert')
         result = value * 1.60934
         self.root.ids.output_label.text = str(result)
 
-    def handle_up_calculate(self, value):
-        result = value + 1
-        self.root.ids.input_label.text = str(result)
-
-    def handle_down_calculate(self, value):
-        result = value - 1
-        self.root.ids.input_label.text = str(result)
+    def handle_increment(self, value, increment):
+        print('increment')
+        result = int(value) + increment
+        print(result)
+        self.root.ids.input_label.value = int(result)
+        # I do not understand why this does not work. I tried .txt and str(result) and I just can't figure it out
 
 
 MilesKilometreConverterApp().run()
