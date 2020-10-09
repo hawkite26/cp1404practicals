cc3f30c567340dd31174b0bb007f7049c7155a4b
Kye Bryce <kye-bryce@hotmail.com>
2020-10-09 20:38:09 +1000

    Add taxi_test.py code for testing taxi

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,15 @@
+"""
+CP1404
+Taxi test file
+"""
+
+
+from prac_08.taxi import Taxi
+
+
+prius1 = Taxi("Prius 1", 100, 1.23)
+prius1.drive(40)
+print(prius1, ", $", prius1.get_fare())
+prius1.start_fare()
+prius1.drive(100)
+print(prius1, ", $", prius1.get_fare())
