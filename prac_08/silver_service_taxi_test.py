5ecc6c9b34e95a30aaa88571bd5a566793c1aff7
Kye Bryce <kye-bryce@hotmail.com>
2020-10-09 22:18:19 +1000

    test silver_service_taxi.py

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,12 @@
+"""
+Test file for silver_service_taxi.py
+"""
+
+
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+test_ride = SilverServiceTaxi("Silver", 100, 4)
+test_ride.drive(50)
+print(test_ride)
+test_ride.start_fare()
+print(test_ride)
