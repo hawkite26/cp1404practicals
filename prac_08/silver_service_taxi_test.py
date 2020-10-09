60aef0bff46602f9a06ad004c2f68e305e1d0319
Kye Bryce <kye-bryce@hotmail.com>
2020-10-10 02:53:05 +1000

    Add extra code

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -7,6 +7,6 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 test_ride = SilverServiceTaxi("Silver", 100, 4)
 test_ride.drive(50)
-print(test_ride)
+print(test_ride, test_ride.get_fare())
 test_ride.start_fare()
 print(test_ride)
