b62d5f3b2c32a6b7aa9432f96f2954fcc96b9790
Kye Bryce <kye-bryce@hotmail.com>
2020-10-09 20:45:56 +1000

    Change taxi fare price to be constant and change taxi_test.py appropriately

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -7,7 +7,7 @@ Taxi test file
 from prac_08.taxi import Taxi
 
 
-prius1 = Taxi("Prius 1", 100, 1.23)
+prius1 = Taxi("Prius 1", 100)
 prius1.drive(40)
 print(prius1, ", $", prius1.get_fare())
 prius1.start_fare()
