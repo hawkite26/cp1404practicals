7962053425137015cc8bc67713868b14f57e42f6
Kye Bryce <kye-bryce@hotmail.com>
2020-10-10 12:22:13 +1000

    Edit silver_service_taxi_test.py code to check more cases and cleanup code in silver_service_taxi.py

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -5,8 +5,8 @@ Test file for silver_service_taxi.py
 
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
-test_ride = SilverServiceTaxi("Silver", 100, 4)
-test_ride.drive(50)
+test_ride = SilverServiceTaxi("Silver", 100, 2)
+test_ride.drive(18)
 print(test_ride, test_ride.get_fare())
 test_ride.start_fare()
 print(test_ride)
